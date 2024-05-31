c38cfe53bc6bc59e0a3f2ddf4130243429343150
Rafayttg <127394474+rafayttg@users.noreply.github.com>
2024-05-31 17:17:47 +0500

    new pages added

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+    //responsive menu
+    $(".menu-bottom").on("click", function() {
+        $(".header").toggleClass("menu-open")
+    });
+    $(".menu-bottom").click(function() {
+        $(this).toggleClass("click")
+    });
